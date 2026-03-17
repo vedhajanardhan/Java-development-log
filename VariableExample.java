442ac60f62e5c8a0cd8bf079b8b456949e23527c
Vedha Janardhan <vedhajanardhan@gmail.com>
2026-03-17 23:26:22 +0530

    Add files via upload

---
+++ VariableExample.java
@@ -0,0 +1,11 @@
+public class VariableExample {
+    public static void main(String[] args) {
+
+        int age = 21;
+        String name = "Ved";
+
+        System.out.println(name);
+        System.out.println(age);
+
+    }
+}
