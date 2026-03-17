dfa30af519c27610f09e5f31194dd6e5281a0f35
Vedha Janardhan <vedhajanardhan@gmail.com>
2026-03-17 23:33:00 +0530

    Add files via upload

---
+++ DatatypeExample.java
@@ -0,0 +1,15 @@
+public class DatatypeExample {
+    public static void main(String[] args) {
+
+        int age = 21;
+        double salary = 50000.5;
+        char grade = 'A';
+        boolean passed = true;
+
+        System.out.println(age);
+        System.out.println(salary);
+        System.out.println(grade);
+        System.out.println(passed);
+
+    }
+}
