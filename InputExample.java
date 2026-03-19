021c6181b96668f5dc599a31bbf431968811d4e3
Vedha Janardhan <vedhajanardhan@gmail.com>
2026-03-19 20:11:41 +0530

    Add files via upload

---
+++ InputExample.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class InputExample {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number:");
+
+        int num = sc.nextInt();
+
+        System.out.println("You entered: " + num);
+
+    }
+}
